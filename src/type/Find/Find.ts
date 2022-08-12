f6905100096d4e99b36472a2690ea393f0328461
Jie <1837924114@qq.com>
2022-08-12 18:41:51 +0800

    第三十一次提交

--- src/type/Find/Find.ts
+++ src/type/Find/Find.ts
@@ -1,11 +1,11 @@
 //定义轮播的数据类型
 export interface FindTypeBanners {
-    data: Array<string>;
+    data: any;
 }
 
 //定义推荐歌单的数据类型
 export interface FindTypeRecommendMusicList {
-    data: Array<string | number>;
+    data: any;
 }
 
 //定义搜索关键字的数据类型
