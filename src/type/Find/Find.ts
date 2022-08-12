5f4fca55c8b43c54d11230763194e9ce33df29f6
Jie <1837924114@qq.com>
2022-08-12 18:40:16 +0800

    第三十次提交

--- src/type/Find/Find.ts
+++ src/type/Find/Find.ts
@@ -2,14 +2,17 @@
 export interface FindTypeBanners {
     data: Array<string>;
 }
+
 //定义推荐歌单的数据类型
 export interface FindTypeRecommendMusicList {
-    data: Array<any>;
+    data: Array<string | number>;
 }
+
 //定义搜索关键字的数据类型
 export interface FindTypeSearchKwords {
     name: any
 }
+
 //定义推荐歌曲的数据类型
 export interface FindTypeRecommendSong {
     data: any
