24234c3d600cced2544854ba25448e981a5e1cc3
Jie <1837924114@qq.com>
2022-08-12 18:22:56 +0800

    第二十六次提交

--- src/type/Find/Find.ts
+++ src/type/Find/Find.ts
@@ -8,7 +8,7 @@ export interface FindTypeRecommendMusicList {
 }
 //定义搜索关键字的数据类型
 export interface FindTypeSearchKwords {
-    name: string
+    name: any
 }
 //定义推荐歌曲的数据类型
 export interface FindTypeRecommendSong {
